934d17be57e0ad9372ec306501ad4f0ea7d46a6e
Kevin Bevan <iambevan@gmail.com>
2021-03-13 04:10:22 +0000

    WIP - bar -chart working without axis

--- components/chart/chart.tsx
+++ components/chart/chart.tsx
@@ -20,123 +20,124 @@ type Data = {
 	name: string
 }
 
-const initialData: Data[] = [
-	{ units: 2150, color: "orange", name: "Cats" },
-	{ units: 4450, color: "blue", name: "Dogs" },
-	{ units: 5900, color: "red", name: "Snakes" },
-	{ units: 7590, color: "green", name: "Pigs" },
-	{ units: 1200, color: "purple", name: "Wolves" },
-	{ units: 8000, color: "pink", name: "Cows" },
-	{ units: 9000, color: "cyan", name: "Lizards" },
-	{ units: 5600, color: "cyan", name: "Bears" },
+let initialData: Data[] = [
+	{
+		name: "foo",
+		units: 32,
+		color: "red",
+	},
+	{
+		name: "bar",
+		units: 67,
+		color: "green",
+	},
+	{
+		name: "baz",
+		units: 81,
+		color: "blue",
+	},
+	{
+		name: "hoge",
+		units: 38,
+		color: "pink",
+	},
+	{
+		name: "piyo",
+		units: 28,
+		color: "purple",
+	},
+	{
+		name: "hogera",
+		units: 59,
+		color: "cyan",
+	},
 ]
-
-const dimensions = {
-	width: 800,
-	height: 450,
-	chartWidth: 700,
-	chartHeight: 350,
-	marginLeft: 100,
-}
-
-export const Chart = (): JSX.Element => {
+export const Chart: React.FC = () => {
+	const dimensions = {
+		width: 800,
+		height: 450,
+		chartWidth: 700,
+		chartHeight: 350,
+		marginLeft: 100,
+		marginBottom: 100,
+	}
 	const svgRef = useRef<SVGSVGElement | null>(null)
+	const [data, setData] = useState<Data[]>(initialData)
+
+	const maxValue = max(data, d => d.units)
+
+	let y = useRef({
+		scale: scaleLinear()
+			.domain([0, maxValue as NumberValue])
+			.range([dimensions.height, 0]),
+	})
+	let x = useRef({
+		scale: scaleBand()
+			.domain(data.map(d => d.name))
+			.range([0, dimensions.width])
+			.padding(0.05),
+	})
+
 	const [selection, setSelection] = useState<null | Selection<
 		SVGSVGElement | null,
 		unknown,
 		null,
 		undefined
 	>>(null)
-	const [data, setData] = useState(initialData)
-
-	const maxValue = max(data, d => d.units)
-
-	let y = scaleLinear()
-		.domain([0, maxValue as NumberValue])
-		.range([dimensions.chartHeight, 0])
-
-	let x = scaleBand()
-		.domain(data.map(d => d.name))
-		.range([0, dimensions.chartWidth])
-		.paddingInner(0.05)
-
-	const yAxis = axisLeft(y)
-		.ticks(5)
-		.tickFormat(t => `${t as number}u`)
-	const xAxis = axisBottom(x).ticks(5)
+	// const yAxis = axisLeft(y.current.scale)
+	// 	.ticks(5)
+	// 	.tickFormat(t => `${t as number}u`)
+	// const xAxis = axisBottom(x.current.scale).ticks(5)
 
 	useEffect(() => {
 		if (!selection) {
 			setSelection(select(svgRef.current))
 		} else {
-			/**
-			 * xAxis group
-			 */
 			selection
 				.append("g")
-				.call(xAxis)
+				.attr("height", dimensions.chartHeight)
+				.attr("width", dimensions.chartWidth)
+				.attr("id", "chart-container")
 				.attr(
 					"transform",
-					`translate(${dimensions.marginLeft}, ${dimensions.chartHeight})`
+					`translate(${
+						dimensions.marginLeft
+					}, ${-dimensions.marginBottom})`
 				)
-				.attr("height", dimensions.height - dimensions.chartHeight)
-				.attr("z-index", 1000)
-				.attr("fill", "salmon")
-
-			/**
-			 * yAxis group
-			 */
-			selection
-				.append("g")
-				.call(yAxis)
-				.attr("transform", `translate(${dimensions.marginLeft}, 0)`)
-
-			/**
-			 * Bars - rects inside g
-			 */
-			selection
-				.append("g")
-				.attr("id", "chart")
-				.attr("transform", `translate(${dimensions.marginLeft}, 0)`)
 				.selectAll("rect")
 				.data(data)
 				.enter()
 				.append("rect")
-				.attr("width", x.bandwidth())
-				.attr("height", 0)
+				.attr("x", d => x.current.scale(d.name) ?? null)
+				.attr("y", d => y.current.scale(d.units))
 				.attr("fill", d => d.color)
-				.attr("x", d => x(d.name) ?? null)
-				.attr("y", dimensions.chartHeight)
-				.transition()
-				.ease(easeLinear)
-				.duration(300)
-				.delay((_, i) => i * 100)
-				.attr("height", d => dimensions.chartHeight - y(d.units))
-				.attr("y", d => y(d.units))
+				.attr("width", x.current.scale.bandwidth())
+				.attr(
+					"height",
+					d => dimensions.chartHeight - y.current.scale(d.units)
+				)
 		}
-		return () => {
-			if (selection) {
-				selection.selectAll("g").remove().exit()
-				selection.selectAll("rect").remove().exit()
-			}
-		}
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [selection])
 
+	/**
+	 * another useEffect with data as its dependency
+	 * runs everytime data changes so updates can be made to the chart
+	 */
 	useEffect(() => {
 		if (selection) {
-			// eslint-disable-next-line react-hooks/exhaustive-deps
-			y = scaleLinear()
+			y.current.scale = scaleLinear()
 				.domain([0, maxValue as NumberValue])
 				.range([0, dimensions.chartHeight])
 
-			// eslint-disable-next-line react-hooks/exhaustive-deps
-			x = scaleBand()
+			x.current.scale = scaleBand()
 				.domain(data.map(d => d.name))
 				.range([0, dimensions.chartWidth])
 				.paddingInner(0.05)
 
 			const rects = selection
-				.selectAll("chart")
+				.select("#chart-container")
 				.selectAll("rect")
 				.data(data)
 
@@ -146,42 +147,41 @@ export const Chart = (): JSX.Element => {
 				.ease(easeElastic)
 				.duration(400)
 				.attr("height", 0)
-				.attr("y", dimensions.height)
+				.attr("y", dimensions.chartHeight)
 				.remove()
 
 			rects
 				.transition()
-				.duration(300)
-				.attr("width", x.bandwidth())
-				.attr("height", d => y(d.units))
+				.delay(300)
+				.attr("x", d => x.current.scale(d.name) ?? null)
+				.attr("y", d => y.current.scale(d.units))
+				.attr("width", x.current.scale.bandwidth())
+				.attr(
+					"height",
+					d => dimensions.chartHeight - y.current.scale(d.units)
+				)
 				.attr("fill", d => d.color)
-				.attr("x", d => x(d.name) ?? null)
 
 			rects
 				.enter()
 				.append("rect")
-				.attr("x", d => x(d.name) ?? null)
-				.attr("width", x.bandwidth())
-				.attr("height", d => dimensions.chartHeight - y(d.units))
+				.attr("x", d => x.current.scale(d.name) ?? null)
+				.attr("width", x.current.scale.bandwidth())
+				.attr("height", 0)
 				.attr("y", dimensions.chartHeight)
 				.transition()
 				.delay(400)
 				.duration(500)
 				.ease(easeElastic)
-				.attr("height", d => dimensions.chartHeight - y(d.units))
-				.attr("y", d => y(d.units))
+				.attr(
+					"height",
+					d => dimensions.height - y.current.scale(d.units)
+				)
+				.attr(
+					"y",
+					d => y.current.scale(d.units) - dimensions.marginBottom
+				)
 				.attr("fill", d => d.color)
-		}
-
-		return () => {
-			if (selection) {
-				selection
-					.selectAll("chart")
-					.selectAll("rect")
-					.data(data)
-					.exit()
-					.remove()
-			}
 		}
 	}, [data])
 
@@ -197,7 +197,7 @@ export const Chart = (): JSX.Element => {
 	const addRandom = (): void => {
 		const dataToBeAdded = {
 			name: randomstring.generate(10),
-			units: Math.floor(Math.random() * 9999),
+			units: Math.floor(Math.random() * 99),
 			color: getRandomColor(),
 		}
 
@@ -225,3 +225,222 @@ export const Chart = (): JSX.Element => {
 		</>
 	)
 }
+
+// import {
+// 	max,
+// 	axisBottom,
+// 	axisLeft,
+// 	NumberValue,
+// 	scaleBand,
+// 	scaleLinear,
+// 	select,
+// 	selectAll,
+// 	Selection,
+// 	easeLinear,
+// 	easeElastic,
+// } from "d3"
+// import { useEffect, useRef, useState } from "react"
+// import randomstring from "randomstring"
+
+// type Data = {
+// 	units: number
+// 	color: string
+// 	name: string
+// }
+
+// const initialData: Data[] = [
+// 	{ units: 2150, color: "orange", name: "Cats" },
+// 	{ units: 4450, color: "blue", name: "Dogs" },
+// 	{ units: 5900, color: "red", name: "Snakes" },
+// 	{ units: 7590, color: "green", name: "Pigs" },
+// 	{ units: 1200, color: "purple", name: "Wolves" },
+// 	{ units: 8000, color: "pink", name: "Cows" },
+// 	{ units: 9000, color: "cyan", name: "Lizards" },
+// 	{ units: 5600, color: "cyan", name: "Bears" },
+// ]
+
+// const dimensions = {
+// 	width: 800,
+// 	height: 450,
+// 	chartWidth: 700,
+// 	chartHeight: 350,
+// 	marginLeft: 100,
+// }
+
+// export const Chart = (): JSX.Element => {
+// 	const svgRef = useRef<SVGSVGElement | null>(null)
+// 	const [selection, setSelection] = useState<null | Selection<
+// 		SVGSVGElement | null,
+// 		unknown,
+// 		null,
+// 		undefined
+// 	>>(null)
+// 	const [data, setData] = useState(initialData)
+
+// 	const maxValue = max(data, d => d.units)
+
+// 	let y = useRef({
+// 		scale: scaleLinear()
+// 			.domain([0, maxValue as NumberValue])
+// 			.range([dimensions.height, 0]),
+// 	})
+// 	let x = useRef({
+// 		scale: scaleBand()
+// 			.domain(data.map(d => d.name))
+// 			.range([0, dimensions.width])
+// 			.padding(0.05),
+// 	})
+
+// 	const yAxis = axisLeft(y.current.scale)
+// 		.ticks(5)
+// 		.tickFormat(t => `${t as number}u`)
+// 	const xAxis = axisBottom(x.current.scale).ticks(5)
+
+// 	useEffect(() => {
+// 		if (!selection) {
+// 			setSelection(select(svgRef.current))
+// 		} else {
+// 			/**
+// 			 * xAxis group
+// 			 */
+// 			selection
+// 				.append("g")
+// 				.call(xAxis)
+// 				.attr(
+// 					"transform",
+// 					`translate(${dimensions.marginLeft}, ${dimensions.chartHeight})`
+// 				)
+// 				.attr("height", dimensions.height - dimensions.chartHeight)
+// 				.attr("z-index", 1000)
+// 				.attr("fill", "salmon")
+
+// 			/**
+// 			 * yAxis group
+// 			 */
+// 			selection
+// 				.append("g")
+// 				.call(yAxis)
+// 				.attr("transform", `translate(${dimensions.marginLeft}, 0)`)
+
+// 			/**
+// 			 * Bars - rects inside g
+// 			 */
+// 			selection
+// 				.append("g")
+// 				.attr("id", "chart")
+// 				.attr("transform", `translate(${dimensions.marginLeft}, 0)`)
+// 				.selectAll("rect")
+// 				.data(data)
+// 				.enter()
+// 				.append("rect")
+// 				.attr("id", "bar")
+// 				.attr("width", x.current.scale.bandwidth())
+// 				.attr("height", 0)
+// 				.attr("fill", d => d.color)
+// 				.attr("x", d => x.current.scale(d.name) ?? null)
+// 				.attr("y", dimensions.chartHeight)
+// 				.transition()
+// 				.ease(easeLinear)
+// 				.duration(300)
+// 				.delay((_, i) => i * 100)
+// 				.attr(
+// 					"height",
+// 					d => dimensions.chartHeight - y.current.scale(d.units)
+// 				)
+// 				.attr("y", d => y.current.scale(d.units))
+// 		}
+// 	}, [selection])
+
+// 	useEffect(() => {
+// 		if (selection) {
+// 			y.current.scale = scaleLinear()
+// 				.domain([0, maxValue as NumberValue])
+// 				.range([0, dimensions.chartHeight])
+
+// 			x.current.scale = scaleBand()
+// 				.domain(data.map(d => d.name))
+// 				.range([0, dimensions.chartWidth])
+// 				.paddingInner(0.05)
+
+// 			const rects = selection.select("chart").selectAll("rect").data(data)
+
+// 			rects
+// 				.exit()
+// 				// .transition()
+// 				// .ease(easeElastic)
+// 				// .duration(400)
+// 				// .attr("height", 0)
+// 				// .attr("y", dimensions.height)
+// 				.remove()
+
+// 			rects
+// 				.transition()
+// 				.duration(300)
+// 				.attr("width", x.current.scale.bandwidth())
+// 				.attr("height", d => y.current.scale(d.units))
+// 				.attr("fill", d => d.color)
+// 				.attr("x", d => x.current.scale(d.name) ?? null)
+
+// 			rects
+// 				.enter()
+// 				.append("bar")
+// 				.attr("x", d => x.current.scale(d.name) ?? null)
+// 				.attr("width", x.current.scale.bandwidth())
+// 				.attr(
+// 					"height",
+// 					d => dimensions.chartHeight - y.current.scale(d.units)
+// 				)
+// 				.attr("y", dimensions.chartHeight)
+// 				.transition()
+// 				.delay(400)
+// 				.duration(500)
+// 				.ease(easeElastic)
+// 				.attr(
+// 					"height",
+// 					d => dimensions.chartHeight - y.current.scale(d.units)
+// 				)
+// 				.attr("y", d => y.current.scale(d.units))
+// 				.attr("fill", d => d.color)
+// 		}
+// 	}, [data])
+
+// 	function getRandomColor(): string {
+// 		let letters = "0123456789ABCDEF"
+// 		let color = "#"
+// 		for (let i = 0; i < 6; i++) {
+// 			color += letters[Math.floor(Math.random() * 16)]
+// 		}
+// 		return color
+// 	}
+
+// 	const addRandom = (): void => {
+// 		const dataToBeAdded = {
+// 			name: randomstring.generate(10),
+// 			units: Math.floor(Math.random() * 9999),
+// 			color: getRandomColor(),
+// 		}
+
+// 		setData([...data, dataToBeAdded])
+// 	}
+
+// 	const removeLast = (): void => {
+// 		if (data.length === 0) {
+// 			return
+// 		} else {
+// 			const slicedData = data.slice(0, data.length - 1)
+// 			setData(slicedData)
+// 		}
+// 	}
+
+// 	return (
+// 		<>
+// 			<svg
+// 				ref={svgRef}
+// 				width={dimensions.width}
+// 				height={dimensions.height}
+// 			/>
+// 			<button onClick={addRandom}>+</button>
+// 			<button onClick={removeLast}>-</button>
+// 		</>
+// 	)
+// }
